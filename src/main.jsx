8a4dca89d8e06630e1bb857e43daa9a00bb778ab
Victor Onyekwere <cydollarsign1@gmail.com>
2024-12-04 17:26:08 +0100

    Installed all dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -5,30 +5,27 @@ import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { extendTheme } from "@chakra-ui/react";
 import { mode } from "@chakra-ui/theme-tools";
-import { BrowserRouter } from "react-router-dom";
 
 const styles = {
   global: (props) => ({
     body: {
-      bg: mode("gray.100", "#000")(props),
-      color: mode("gray.800", "whiteAlpha.900")(props),
+      bg: mode("gray.100", "#fff")(props),
+      color: mode("gray.800", "#000")(props),
     },
   }),
 };
 
 const config = {
-  initialColorMode: "dark",
+  initialColorMode: "light", // Set to light mode
   useSystemColorMode: false,
 };
 
-const theme = extendTheme({ config }); //Add styles as a 2nd parameter for custome style override
+const theme = extendTheme({ config, styles }); // Add styles for custom style override
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </ChakraProvider>
   </React.StrictMode>
 );
