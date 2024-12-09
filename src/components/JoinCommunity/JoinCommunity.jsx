b461cd23aa5e8827f17019c869865cc5322fcad5
Victor Onyekwere <cydollarsign1@gmail.com>
2024-12-09 01:38:08 +0100

    Deployment Adjustment

--- src/components/JoinCommunity/JoinCommunity.jsx
+++ src/components/JoinCommunity/JoinCommunity.jsx
@@ -1,6 +1,6 @@
 import { Flex, Text, Box, Image, Link } from "@chakra-ui/react"
 import { Link as RouterLink } from "react-router-dom"
-import RoundImageContainer from "./roundImageContainer.jsx"
+// import RoundImageContainer from "./roundImageContainer.jsx"
 import LinkButton from "../LinkButton"
 import HeadingCustom from "../HeadingCustom"
 
@@ -12,7 +12,7 @@ const JoinCommunity = () => {
             </Flex>
             <Text mt={"20px"} textAlign={"center"} fontSize={{xl: "24px"}}>Join our community of over 1000+ founders, developers, and tech junkies in general.<br />
                 Be inspired by people who live to inspire!</Text>
-            <Box my={"20px"} display={{ xl: "none", }}> <RoundImageContainer size="13rem" image={"/belba.png"} y={0} color={"#8300FF"} /> </Box>
+            {/* <Box my={"20px"} display={{ xl: "none", }}> <RoundImageContainer size="13rem" image={"/belba.png"} y={0} color={"#8300FF"} /> </Box> */}
             <Box
                 h="320px"
                 display={{ base: "none", md: "none", lg: "none", xl: "flex", }}
@@ -26,13 +26,13 @@ const JoinCommunity = () => {
                 backgroundPosition="center"
                 backgroundRepeat="no-repeat"
                 zIndex={3} >
-                <RoundImageContainer image={"/belba.png"} y={135} color={"#8300FF"} />
+                {/* <RoundImageContainer image={"/belba.png"} y={135} color={"#8300FF"} />
                 <RoundImageContainer image={"/belba.png"} y={-40} color={"#2F6C62"} />
                 <RoundImageContainer image={"/belba.png"} y={70} color={"#FF3B30"} />
                 <RoundImageContainer image={"/belba.png"} y={70} color={"#007AFF"} />
                 <RoundImageContainer image={"/belba.png"} y={70} color={"#FF3B30"} />
                 <RoundImageContainer image={"/belba.png"} y={-40} color={"teal"} />
-                <RoundImageContainer image={"/belba.png"} y={135} color={"#FF9500"} />
+                <RoundImageContainer image={"/belba.png"} y={135} color={"#FF9500"} /> */}
             </Box>
             <LinkButton border="2px solid black" text="Register Now" arrow={true} link="https://www.belsoftsystems.com/" color="#A649FF" />
 
