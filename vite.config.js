60ae1202d9186a27d552bf9fe7c0960030ad893e
Victor Onyekwere <cydollarsign1@gmail.com>
2024-12-09 01:18:07 +0100

    Deployment Adjustment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
+
   plugins: [react()],
+
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    target: 'esnext',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom']
+        }
+      }
+    }
+  },
+
+  server: {
+    port: 5173,
+    open: true,
+    host: true
+  },
+
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  },
+
+  optimizeDeps: {
+    include: ['react', 'react-dom']
+  }
 })
